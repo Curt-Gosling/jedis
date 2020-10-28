1ebb8df092a784854960c239b1f728c7abe3102c
Curt <707489624@qq.com>
2020-10-28 20:43:56 +0800

    001

---
+++ abc.java
@@ -0,0 +1,22 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Protocol;
+
+public class abc {
+    public static void main(String[] args) {
+        //声明Linux服务器IP地址
+        String host = "192.168.6.102";
+        //声明Redis端口号
+        int port = Protocol.DEFAULT_PORT;
+        //创建连接池对象
+        JedisPool jedisPool = new JedisPool(host, port);
+        //获取Jedis对象连接Redis
+        Jedis jedis = jedisPool.getResource();
+        //执行具体操作
+        String ping = jedis.ping();
+        System.out.println(ping);
+        //关闭连接
+        jedisPool.close();
+
+    }
+}
